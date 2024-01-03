7136bc27b14054ee0636476681e32c2b63966b65
ouyangyu <2455356027@qq.com>
2024-01-03 17:57:59 +0800

    init

--- src/main/java/com/me/demo21/Demo21Application.java
+++ src/main/java/com/me/demo21/Demo21Application.java
@@ -2,8 +2,10 @@ package com.me.demo21;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.redis.outbound.ExpressionArgumentsStrategy;
 
 import java.lang.reflect.AccessFlag;
+import java.util.Collection;
 
 @SpringBootApplication
 public class Demo21Application {
@@ -13,9 +15,15 @@ public class Demo21Application {
         System.out.print("dsd");
         Class<UnknownError> unknownErrorClass = UnknownError.class;
         for (AccessFlag accessFlag : unknownErrorClass.accessFlags()) {
-            accessFlag.locations()
+            for (AccessFlag.Location location : accessFlag.locations()) {
+                System.out.println(location);
+            }
         }
-
+        UnknownError.class.accessFlags()
+                .stream()
+                .map(AccessFlag::locations)
+                .flatMap(Collection::stream)
+                .forEach(System.out::println);
 
     }
 
